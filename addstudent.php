f3e693657a7562f7e2252b7539cf28b2481b34a4
manel2025 <wardamanelkh@gmail.com>
2025-11-23 03:39:52 +0100

    Updated PHP and HTML files

---
+++ addstudent.php
@@ -0,0 +1,21 @@
+<?php
+// Check if the form has been submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $id = $_POST['studentId'];
+    $last = $_POST['lastName'];
+    $first = $_POST['firstName'];
+    $email = $_POST['email'];
+
+    // Save inside a simple text file (for beginners)
+    $file = fopen("students.txt", "a");
+    fwrite($file, "$id, $last, $first, $email\n");
+    fclose($file);
+
+    echo "<h2>Student added successfully!</h2>";
+    echo "<a href='addstudent.html'>Go Back</a>";
+} 
+else {
+    echo "Invalid request!";
+}
+?>
